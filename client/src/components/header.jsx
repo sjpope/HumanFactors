859f9c1536219938e0fec62c62b4a9276b736217
Jarrett <115436271+JarrettFontenot@users.noreply.github.com>
2024-03-08 17:34:41 -0600

    React app start

---
+++ client/src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>EasyBook</h1>
+            <nav>
+                <ul>
+                    <li>Sign Up/Log In</li> 
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
