b2c492aa17c1e270d0ec330d4acc4c49de9576cf
Jarrett <115436271+JarrettFontenot@users.noreply.github.com>
2024-03-21 10:12:57 -0500

    updated side bar

--- client/src/components/sideBar.jsx
+++ client/src/components/sideBar.jsx
@@ -3,13 +3,24 @@ import React from "react";
 function SideBar() {
     return (
         <div className = "sideBar">
-            <h3>Health Rating</h3>
             <div className="sideBarContent">
+                <h3>Healthy Foods</h3>
                 <form>
                     <input type="checkbox" id="healthCheckBox"></input>
-                    <label for="healthCheckBox">Very Healthy Choices</label><br/>
-                    <input type="checkbox" id="unhealthyCheckBox"></input>
-                    <label for="healthCheckBox">Not The Best</label><br/>
+                    <label for="healthCheckBox">Healthy Choices</label><br/>
+                </form>
+                <h3>Price Rating</h3>
+                <form>
+                    <button id="cheapButton">$$</button>
+                    <button id="middleButton">$$$</button>
+                    <button id="expensiveButton">$$$$</button>
+                </form>
+                <h3>Food Allergies</h3>
+                <form>
+                    <input type="checkbox" id="glutenAllergy"></input>
+                    <label for="glutenAllergy">Gluten Free</label><br/>
+                    <input type="checkbox" id="nutAllergy"></input>
+                    <label for="nutAllergy">Nut Allergy Safe</label><br/>
                 </form>
             </div>
         </div>
