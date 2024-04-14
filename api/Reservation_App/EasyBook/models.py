e027326013bbcf29721b46db5e9d05512d679841
Samuel J Pope <spope@inrsenterprises.com>
2024-04-13 23:12:29 -0500

    Add models for Restaurant, MenuItem, and ReservationSlot

--- api/Reservation_App/EasyBook/models.py
+++ api/Reservation_App/EasyBook/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=500)
+    cuisine_type = models.CharField(max_length=100)
+    health_rating = models.DecimalField(max_digits=2, decimal_places=1)
+    price_level = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class ReservationSlot(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.date_time}"
+
--- api/Reservation_App/EasyBook/views.py
+++ api/Reservation_App/EasyBook/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Restaurant, MenuItem
 
 def home_data_api_view(request):
-    data = {'message': 'Placeholder data for home page'}
+    
+    # query models, send to frontend
+    restaurants = Restaurant.objects.all()  
+    data = {
+        'message': 'Placeholder data for home page',
+        'restaurants': list(restaurants.values())  # creates list of dictionaries, [{'x': 1}, {'y': 2]
+    }
     return JsonResponse(data)
 
 def results_data_api_view(request):
-    data = {'message': 'Placeholder data for results page'}
+    # Assume you pass filters via query parameters, for example
+    search_query = request.GET.get('search', '')
+    filter_results = MenuItem.objects.filter(name__icontains=search_query)  
+    data = {
+        'message': 'Placeholder data for results page',
+        'results': list(filter_results.values())
+    }
     return JsonResponse(data)
-
