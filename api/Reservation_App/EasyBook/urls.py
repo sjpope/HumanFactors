5b4c225dd46fa2a67fd6e6209e533bb93fd01425
Samuel J Pope <spope@inrsenterprises.com>
2024-04-14 02:13:14 -0500

    add RestaurantListView and RestaurantDetailView to urls, update search endpoint

--- api/Reservation_App/EasyBook/urls.py
+++ api/Reservation_App/EasyBook/urls.py
@@ -5,14 +5,16 @@ from .views import *
 app_name = 'EasyBook'
 
 urlpatterns = [
-    path('', home_data_api_view, name='home_data_api'),
-    path('results/', results_data_api_view, name='results_data_api'),
     
-    path('search/', search_restaurants, name='search_restaurants'),
+    path('', RestaurantListView.as_view(), name='home_data_api'),
+    path('restaurant/<int:restaurant_id>/', RestaurantDetailView.as_view(), name='restaurant_detail'),
+
+    path('search/', SearchRestaurantsAPIView.as_view(), name='search_restaurants'),
     
     path('profile/', DiningProfileAPIView.as_view(), name='dining-profile'),
     path('recommendations/', RecommendationAPIView.as_view(), name='recommendations'),
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
